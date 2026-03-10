3c02f0f97dd4c37b4e1acf0eb900269b61c9dfa6
vimbeldum <shwetanshu1992@gmail.com>
2026-03-10 14:20:28 +0530

    chore: update openwire to v0.7.5

--- Formula/openwire.rb
+++ Formula/openwire.rb
@@ -1,8 +1,8 @@
 class Openwire < Formula
   desc "P2P encrypted messenger for local networks with zero configuration"
   homepage "https://github.com/vimbeldum/openwire"
-  url "https://github.com/vimbeldum/openwire/archive/refs/tags/v0.7.4.tar.gz"
-  sha256 "d66d923822ccd871bdbd598bec79b5f4bc9924c9645405deecbd67274852f359"
+  url "https://github.com/vimbeldum/openwire/archive/refs/tags/v0.7.5.tar.gz"
+  sha256 "ec774ec5fcf92e03f4e4e05047e9f672bfd482f205edc5e858e31f121a1ee5d3"
   license "MIT"
   head "https://github.com/vimbeldum/openwire.git", branch: "master"
 
