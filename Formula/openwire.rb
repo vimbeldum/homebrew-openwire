909ec1fb236b32542888703c135aa2f95a52854d
vimbeldum <shwetanshu1992@gmail.com>
2026-03-10 17:09:10 +0530

    chore: bump to v0.9.1 — bug fixes

--- Formula/openwire.rb
+++ Formula/openwire.rb
@@ -1,8 +1,8 @@
 class Openwire < Formula
   desc "Decentralized P2P local network messenger - encrypted, anonymous, zero-config"
   homepage "https://github.com/vimbeldum/openwire"
-  url "https://github.com/vimbeldum/openwire/archive/refs/tags/v0.9.0.tar.gz"
-  version "0.9.0"
+  url "https://github.com/vimbeldum/openwire/archive/refs/tags/v0.9.1.tar.gz"
+  version "0.9.1"
   license "MIT"
 
   depends_on "rust" => :build
